8d0da904716153f4866fc9c477da57dbdfed4c4b
vikram gupta <myselfvikramgupta@gmail.com>
2022-04-10 21:33:19 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='container d_flex_space_between'>
+        <h1 className="header_title">The Real Dairy</h1>
+        <button className="btn btn_primary">Login</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
